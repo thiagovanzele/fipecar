ec26ca850357d3403012f63c75c3595d9d545dda
Thiago Vanzele <thiagovanzele@gmail.com>
2024-06-17 17:50:40 -0300

    Funções que filtram e mostram os modelos desejados

--- src/main/java/br/com/vanzele/application/Principal.java
+++ src/main/java/br/com/vanzele/application/Principal.java
@@ -2,8 +2,10 @@ package br.com.vanzele.application;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import br.com.vanzele.application.model.entities.Dados;
+import br.com.vanzele.application.model.entities.Modelos;
 import br.com.vanzele.model.exceptions.FilterException;
 import br.com.vanzele.model.services.ConsomeApiService;
 import br.com.vanzele.model.services.CriaObjetoService;
@@ -17,18 +19,26 @@ public class Principal {
 	
 	public static void exibeMenu() {
 		System.out.println("\n[1] - Carros\n[2] - Motos\n[3] - Caminhões");
-			
-		String endereco = URL_BASE + escolheTipo() + "/marcas/";
+		
+		String tipoVeiculo = escolheTipo();
+		String endereco = URL_BASE + tipoVeiculo + "/marcas/";
 		String json = ConsomeApiService.retornaJson(endereco);
 		List<Dados> marcas = service.retornaListaDados(json, Dados.class);
+		System.out.println("\n|" + tipoVeiculo.toUpperCase() + "|\n");
 		
 		Dados marcaFiltrada = escolheMarca(marcas);
 		
 		endereco += marcaFiltrada.codigo() + "/modelos/";
 		
 		json = ConsomeApiService.retornaJson(endereco);
-		System.out.println(json);
+		Modelos modelos = service.retornaDado(json, Modelos.class);
+		List<Dados> modelosDisponiveis = escolheNomeDoVeiculo(modelos);
+		exibeModelos(modelosDisponiveis);
 		
+
+		endereco += escolheCodigoVeiculo();
+		json = ConsomeApiService.retornaJson(endereco);
+		System.out.println(json);
 	}
 	
 	public static String escolheTipo() {
@@ -53,5 +63,23 @@ public class Principal {
 		return listaMarcas.stream().filter(n -> n.nome().toLowerCase().contains(marcaSelecionada.toLowerCase())).findFirst().orElseThrow(() -> new FilterException("Não encontrado"));
 	}
 	
+	public static List<Dados> escolheNomeDoVeiculo(Modelos modelos) {
+		System.out.print("Modelo: ");
+		String modeloSelecionado = sc.nextLine();
+		
+		return modelos.modelos().stream().filter(n -> n.nome().toLowerCase().contains(modeloSelecionado.toLowerCase())).collect(Collectors.toList());
+	}
 	
+	public static String escolheCodigoVeiculo() {
+		System.out.println("\nCódigo: ");
+		String codigo = sc.nextLine();
+		return codigo + "/anos/";
+	}
+	
+	public static void exibeModelos (List<Dados> modelos) {
+		for (Dados m : modelos) {
+			System.out.print("[Codigo: " + m.codigo() + "]");
+			System.out.println(" - " + m.nome());
+		}
+	}
 }

d53dc7dc0a7534dc89224b3af8ff822450ac6f4b
Thiago Vanzele <thiagovanzele@gmail.com>
2024-06-17 16:45:50 -0300

    Criação do projeto

---
+++ src/main/java/br/com/vanzele/FipecarApplication.java
@@ -0,0 +1,13 @@
+package br.com.vanzele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipecarApplication.class, args);
+	}
+
+}

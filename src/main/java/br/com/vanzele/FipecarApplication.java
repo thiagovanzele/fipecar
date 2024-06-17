82ad9225c64d9125c9773fca75fe9896f19e9361
Thiago Vanzele <thiagovanzele@gmail.com>
2024-06-17 16:51:17 -0300

    Criando a classe principal para exibir menu

--- src/main/java/br/com/vanzele/FipecarApplication.java
+++ src/main/java/br/com/vanzele/FipecarApplication.java
@@ -1,13 +1,21 @@
 package br.com.vanzele;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.vanzele.application.Principal;
+
 @SpringBootApplication
-public class FipecarApplication {
+public class FipecarApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FipecarApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal.exibeMenu();
 	}
 
 }
---
+++ src/main/java/br/com/vanzele/application/Principal.java
@@ -0,0 +1,17 @@
+package br.com.vanzele.application;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static void exibeMenu() {
+		
+		System.out.println("\n[1] - Carros\n[2] - Motos\n[3] - Caminhões");
+		System.out.print("\nSua opção: ");
+		Integer opcao = sc.nextInt();
+		
+		
+	}
+}
